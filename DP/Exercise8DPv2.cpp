ed9b19308535b44ead164502c91a5e2d4e4aa0af
JorgeMamaniEsc <jorgeescobedo221@gmail.com>
2026-01-16 15:45:32 -0500

    version con knapsack 0/1

---
+++ DP/Exercise8DPv2.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+//using namespace __gnu_pbds;
+#define int long long
+#define f(i,a,b,c) for(int i=a;i<b;i+=c)
+#define rf(i,a,b,c) for(int i=a;i>=b;i-=c)
+#define vi vector<int>
+#define vd vector<long double>
+#define vs vector<string>
+#define vc vector<char>
+#define vvi vector<vector<int>>
+#define vb vector<bool>
+#define vp vector<pair<int,int>>
+#define vvp vector<vp>
+#define pb push_back
+#define sz(v) ((int)(v).size())
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define endl '\n'
+#define i128 __int128
+#define pii pair<int,int>
+#define pi 3.14159265358979323846
+#define sq2 (sqrt(2.0))
+#define ld long double
+//typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+
+//template <typename T>
+//using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+const int MOD=1e9+7;
+
+
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt=1;
+    //cin>>tt;
+    while(tt--){
+        int n;cin>>n;
+        int s=n*(n+1)/2;
+        if(s%2){
+            cout<<0<<endl;
+            continue;
+        }
+        s/=2;
+        vvi dp(n,vi(s+1,0));
+        dp[0][0]=1;
+        f(i,1,n,1){
+            f(j,0,s+1,1){
+                dp[i][j]=dp[i-1][j];
+                int ne=j-i;
+                if(ne>=0)dp[i][j]=(dp[i][j]+dp[i-1][ne])%MOD;
+            }
+        }
+        cout<<dp[n-1][s]<<endl;
+    }
+    return 0;
+}
