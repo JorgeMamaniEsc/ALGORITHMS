d0df496a32ff4ecc1f084a022424a8ddbec5f8fa
JorgeMamaniEsc <jorgeescobedo221@gmail.com>
2026-03-21 12:53:17 -0500

    plantilla para funcion z

---
+++ STRINGS/FUNCIONZ.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+//using namespace __gnu_pbds;
+#define int long long
+#define f(i,a,b,c) for(int i=a;i<b;i+=c)
+#define rf(i,a,b,c) for(int i=a;i>=b;i-=c)
+#define dbg(x) cerr<<#x<<" = "<<(x)<<endl
+#define raya cerr<<" ====================== "<<endl
+#define vi vector<int>
+#define vd vector<long double>
+#define vs vector<string>
+#define vc vector<char>
+#define vvi vector<vector<int>>
+#define vvvi vector<vector<vector<int>>>
+#define vb vector<bool>
+#define vp vector<pair<int,int>>
+#define vvp vector<vp>
+#define pb push_back
+#define sz(v) ((int)(v).size())
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define endl '\n'
+#define i128 __int128
+#define pii pair<int,int>
+#define pi 3.14159265358979323846
+#define sq2 (sqrt(2.0))
+#define ld long double
+//typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+
+//template <typename T>
+//using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+const ld eps=1e-9;
+const int MOD=998244353;
+const int inf=5000000000000000000;
+int mp(int b,int e){
+    int s=1;
+    while(e>0){
+        if(e&1) s=(s*b)%MOD;
+        b=(b*b)%MOD;
+        e>>=1;
+    }
+    return s;
+}
+int dx[4]={1,-1,0,0};
+int dy[4]={0,0,1,-1};
+
+void zzz(string &s,vi &z){
+    int n=sz(s);
+    int l=0,r=0;
+    f(i,1,n,1){
+        if(i<=r) z[i]=min(r-i+1,z[i-l]);
+        while(i+z[i]<n&&s[z[i]]==s[i+z[i]])z[i]++;
+        if(i+z[i]-1>r){
+            l=i;
+            r=i+z[i]-1;
+        }
+    }
+    z[0]=n;
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt=1;cin>>tt;
+    while(tt--){
+        int n;cin>>n;
+        string s;cin>>s;
+        vi z(n);
+        zzz(s,z);
+    }
+    return 0;
+}
