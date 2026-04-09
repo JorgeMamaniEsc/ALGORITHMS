4890e7238da1e3c24112d2ca3348b04bd44399eb
JorgeMamaniEsc <jorgeescobedo221@gmail.com>
2026-04-08 23:06:50 -0500

    toposort

---
+++ Graphs/Toposort.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+//using namespace __gnu_pbds;
+#define int long long
+#define f(i,a,b,c) for(int i=a;i<b;i+=c)
+#define rf(i,a,b,c) for(int i=a;i>=b;i-=c)
+#define dbg(x) cerr<<#x<<" = "<<(x)<<endl
+#define raya cerr<<" ====================== "<<endl
+#define vi vector<int>
+#define vd vector<long double>
+#define vs vector<string>
+#define vc vector<char>
+#define vvi vector<vector<int>>
+#define vvvi vector<vector<vector<int>>>
+#define vb vector<bool>
+#define vp vector<pair<int,int>>
+#define vvp vector<vp>
+#define pb push_back
+#define sz(v) ((int)(v).size())
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define endl '\n'
+#define i128 __int128
+#define pii pair<int,int>
+#define pi 3.14159265358979323846
+#define sq2 (sqrt(2.0))
+#define ld long double
+//typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+
+//template <typename T>
+//using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+const ld eps=1e-9;
+const int MOD=1000000007;
+const int inf=1000000000000000000;
+int mp(int b,int e){
+    int s=1;
+    while(e>0){
+        if(e&1) s=(s*b)%MOD;
+        b=(b*b)%MOD;
+        e>>=1;
+    }
+    return s;
+}
+int dx[4]={1,-1,0,0};
+int dy[4]={0,0,1,-1};
+
+const pii no={-1,-1};
+
+int lcm(int a,int b){
+    return (a*b)/__gcd(a,b);
+}
+int sgn(int x){
+    if(x==0) return 0;
+    return (x<0?-1:1);
+}
+bool choque(pii a,pii b){
+    if(a>b) swap(a,b);
+    auto [l1,r1]=a;
+    auto [l2,r2]=b;
+    if(r1>=l2) return true;
+    return false;
+}
+
+int mex(vi &v,int n){
+    vi fre(n+2,0);
+    f(i,0,n,1) if(v[i]>=0&&v[i]<=n+1) fre[v[i]]++;
+    f(i,0,n+2,1) if(fre[i]==0) return i;
+}
+
+int ask(int l,int r){
+    cout<<"? "<<l<<" "<<r<<endl;
+    cout.flush();
+    int x;cin>>x;
+    return x;
+}
+int damek(int x){
+    int t=0;
+    while(x%2==0){
+        x/=2;
+        t++;
+    }
+    return t;
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt=1;//cin>>tt;
+    while(tt--){
+        int n,m;cin>>n>>m;
+        vvi ady(n);
+        vi gr(n,0);
+        f(i,0,m,1){
+            int a,b;cin>>a>>b;
+            a--;b--;
+            ady[a].pb(b);
+            gr[b]++;
+        }
+        vi dp(n,-inf),parent(n,-1);
+        dp[0]=1;
+        queue<int> q;
+        f(i,0,n,1) if(gr[i]==0) q.push(i);
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            for(int v:ady[u]){
+                gr[v]--;
+                if(gr[v]==0) q.push(v);
+                if(dp[u]==-inf) continue;
+                if(dp[u]+1>dp[v]){
+                    dp[v]=dp[u]+1;
+                    parent[v]=u;
+                }
+            }
+        }
+        if(dp[n-1]==-inf){
+            cout<<"IMPOSSIBLE"<<endl;
+            continue;
+        }
+        vi ans={n-1};
+        int u=n-1;
+        while(u!=0){
+            u=parent[u];
+            ans.pb(u);
+        }
+        cout<<sz(ans)<<endl;
+        reverse(all(ans));
+        for(int x:ans) cout<<x+1<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
