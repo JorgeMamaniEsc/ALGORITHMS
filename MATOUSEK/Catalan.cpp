b791b9b74c878cb8e483316bb4804c6a4f6060ab
JorgeMamaniEsc <jorgeescobedo221@gmail.com>
2026-08-24 21:53:29 -0500

    update: barras con k grande

--- MATOUSEK/Catalan.cpp
+++ MATOUSEK/Catalan.cpp
@@ -2,14 +2,20 @@
 //#include <ext/pb_ds/assoc_container.hpp>
 //#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+
 //using namespace __gnu_pbds;
 #define int long long
-#define f(i,a,b,c) for(int i=a;i<b;i+=c)
-#define rf(i,a,b,c) for(int i=a;i>=b;i-=c)
+#define rep(i,a,b,c) for(int i=a;i<b;i+=c)
+#define nrep(i,a,b,c) for(int i=a;i>=b;i-=c)
+#define dbg(x) cerr<<#x<<" = "<<(x)<<endl
+#define raya cerr<<" ====================== "<<endl
 #define vi vector<int>
 #define vd vector<long double>
+#define vvd vector<vd>
+#define vvvd vector<vvd>
 #define vs vector<string>
 #define vc vector<char>
+#define vvc vector<vector<char>>
 #define vvi vector<vector<int>>
 #define vvvi vector<vector<vector<int>>>
 #define vb vector<bool>
@@ -25,42 +31,27 @@ using namespace std;
 #define pi 3.14159265358979323846
 #define sq2 (sqrt(2.0))
 #define ld long double
+#define vmp vector<map<int,int>>
 //typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
 
 //template <typename T>
 //using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+const ld eps=1e-9;
+const int MOD=998244353;
+//const int MOD=1000000007;
+//const int MOD=1000003;
+//const int inf=2000000000000000000;
+const int inf=1000000000000000000LL;
 
-const int MOD=1000000007;
-const int inf=10000000000000000;
-
-int dx[4]={1,-1,0,0};
-int dy[4]={0,0,1,-1};
-
-
-
-//NUMEROS DE CATALAN
-//Cuentan estructuras combinatorias recursivas donde el orden importa pero la estructura de agrupacion cambia,
-//se denotan Cat0,Cat1,Cat2,...
-//
-//TABULACION PARA DARSE CUENTA RAPIDO
-//N:   0 1 2 3 4  5  6
-//Cat: 1 1 2 5 14 42 132
-//
-//USOS:
-//Numero de formas de parentizar:
-//a1+a2+...+an
-//A u B u C ... u Z
-//Expresiones binarias
-//
-//Usamos Cat(n-1)
-//
-//Secuencias de parentesis validos, numeros de strings balanceados con n pares
-//Numero de arboles binarios con n nodos
-//BSTs distintos con n claves
-//Caminos de (0,0) a (n,n) sin cruzar la diagonal
-
-
-
+int mpv1(int b,int e){
+    int s=1;
+    while(e>0){
+        if(e&1) s=(s*b);
+        b=(b*b);
+        e>>=1;
+    }
+    return s;
+}
 int mp(int b,int e){
     int s=1;
     while(e>0){
@@ -71,13 +62,226 @@ int mp(int b,int e){
     return s;
 }
 
-const int mx=3e5+5;
+int dx[4]={1,-1,0,0};
+int dy[4]={0,0,1,-1};
+char mov[4]={'D','U','R','L'};
+
+const pii no={-1,-1};
+//const pii no={-inf,-inf};
+
+int lcm(int a,int b){
+    return a/__gcd(a,b)*b;
+}
+int sgn(int x){
+    if(x==0) return 0;
+    return (x<0?-1:1);
+}
+bool choque(pii a,pii b){
+    if(a>b) swap(a,b);
+    auto [l1,r1]=a;
+    auto [l2,r2]=b;
+    if(r1>=l2) return true;
+    return false;
+}
+
+int mex(vi &v){
+    int n=sz(v);
+    vi fre(n+2,0);
+    rep(i,0,n,1) if(v[i]>=0&&v[i]<=n+1) fre[v[i]]++;
+    rep(i,0,n+2,1) if(fre[i]==0) return i;
+}
+
+int damek(int x){
+    int t=0;
+    while(x%2==0){
+        x/=2;
+        t++;
+    }
+    return t;
+}
+int disth(pii a,pii b){
+    auto [a1,a2]=a;
+    auto [b1,b2]=b;
+    return abs(a1-b1)+abs(a2-b2);
+}
+bool esprimo(int n){
+    if(n==1) return false;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0) return false;
+    }
+    return true;
+}
+vi damediv(int n){
+    vi ans;
+    for(int i=1;i*i<=n;i++){
+        if(n%i) continue;
+        ans.pb(i);
+        if(i*i!=n) ans.pb(n/i);
+    }
+    return ans;
+}
+
+vi damepri(int n){
+    vi ans;
+    int nn=n;
+    for(int i=2;i*i<=n;i++){
+        if(nn%i)continue;
+        ans.pb(i);
+        while(nn%i==0) nn/=i;
+    }
+    if(nn>1) ans.pb(nn);
+    return ans;
+}
+
+bool mismarecta(pii p1,pii p2,pii p3){
+    auto [x1,y1]=p1;
+    auto [x2,y2]=p2;
+    auto [x3,y3]=p3;
+    return (((y2-y1)*(x3-x1))==((y3-y1)*(x2-x1)));
+}
+
+int disteu(pii p1,pii p2){
+    auto [x1,y1]=p1;
+    auto [x2,y2]=p2;
+    return (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
+}
+
+ld raizeu(pii p1,pii p2){
+    auto [x1,y1]=p1;
+    auto [x2,y2]=p2;
+    return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+}
+int pcruz(pii o,pii p1,pii p2){
+    auto [x1,y1]=p1;
+    auto [x2,y2]=p2;
+    auto [o1,o2]=o;
+    return (x1-o1)*(y2-o2)-(y1-o2)*(x2-o1);
+}
+
+int next2(int n){
+    int x=1;
+    while(x<n) x<<=1ll;
+    return x;
+}
+int sd(int x){
+    int t=0;
+    while(x>0){
+        t+=x%10;
+        x/=10;
+    }
+    return t;
+}
+int tam10(int x){
+    int t=0;
+    if(x==0) return 1;
+    while(x>0){
+        x/=10;
+        t++;
+    }
+    return t;
+}
+
+
+
+string sumastring(string a,string b){
+    reverse(all(a));
+    reverse(all(b));
+    int n=max(sz(a),sz(b));
+    int c=0;
+    string ans;
+    rep(i,0,n,1){
+        int x=(sz(a)<=i?0:a[i]-'0'),y=(sz(b)<=i?0:b[i]-'0');
+        int t=x+y+c;
+        char d=(t%10)+'0';
+        ans.pb(d);
+        c=t/10;
+    }
+    if(c>0) ans.pb(c+'0');
+    reverse(all(ans));
+    return ans;
+}
+
+pii intersec(pii p1,pii p2){
+    if(!choque(p1,p2)) return no;
+    auto [x1,y1]=p1;
+    auto [x2,y2]=p2;
+    int a=max(x1,x2);
+    int b=min(y1,y2);
+    return {a,b};
+}
+
+
+
+string apqpqpqpa(int x){
+    string ans;
+    nrep(j,29,0,1){
+        if((1<<j)&x) ans.pb('1');
+        else if(sz(ans))ans.pb('0');
+    }
+    return ans;
+}
+
+int raiz1(int x){
+    int k=(int)sqrt(x);
+    if(k*k==x) return k;
+    return -1;
+}
+
+pii juntar(pii p1,pii p2){
+    if(p2<p1) swap(p1,p2);
+    pii p={min(p1.first,p2.first),max(p1.second,p2.second)};
+    return p;
+}
+
+int piso(int x,int y){
+    if(x>=0) return x/y;
+    return (x-y+1)/y;
+}
+
+int techo(int x,int y){
+    if(x>=0) return (x+y-1)/y;
+    return x/y;
+}
+
+
+int conv(string x){
+    int n=sz(x),t=0;
+    rep(i,0,n,1) if(x[i]=='1')t+=(1<<i);
+    return t;
+}
+
+vi cambase(int x,int b){
+    vi v;
+    if(x==0) v.pb(0);
+    while(x>0){
+        int c=x%b;
+        v.pb(c);
+        x/=b;
+    }
+    reverse(all(v));
+    return v;
+}
+
+char xo(char c){
+    if(c=='1') return '0';
+    return '1';
+}
+
+struct cosa{
+    int l,r,x;
+    bool operator<(const cosa &o) const{
+        if(l!=o.l) return l<o.l;
+        return r<o.r;
+    }
+};
+
+const int mx=5e5+5;
 int fact[mx],inv[mx];
 void ini(){
     fact[0]=1;
-    f(i,1,mx,1) fact[i]=(fact[i-1]*i)%MOD;
+    rep(i,1,mx,1) fact[i]=(fact[i-1]*i)%MOD;
     inv[mx-1]=mp(fact[mx-1],MOD-2);
-    rf(i,mx-2,0,1) inv[i]=(inv[i+1]*(i+1))%MOD;
+    nrep(i,mx-2,0,1) inv[i]=(inv[i+1]*(i+1))%MOD;
 }
 int com(int n,int k){
     if(k<0||k>n) return 0;
@@ -94,28 +298,26 @@ int cat(int n){
 int barras(int n,int k){
     return com(n+k-1,k-1);
 }
-
-//version dp
-int precat(vi &cata){
-    int n=sz(cata)-1;
-    cata[0]=1;
-    f(i,1,n+1,1){
-        cata[i]=0;
-        f(j,0,i,1) cata[i]=(cata[i]+cata[j]*cata[i-1-j]%MOD)%MOD;
-    }
+int barrasgrandes(int n,int k){
+    if(n==0) return 1;
+    int m=(n+k-1)%MOD,t=1;
+    rep(i,0,n,1) t=(t*(m-i))%MOD;
+    return (t*inv[n])%MOD;
 }
+void mikunakano(){
 
-
-
-
+}
 
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int tt=1;cin>>tt;
+    cout.precision(10);
+	cout<<fixed;
+    int tt=1;
+    cin>>tt;
     ini();
     while(tt--){
-        //
+        mikunakano();
     }
 
     return 0;
--- cp.cpp
+++ cp.cpp
@@ -34,293 +34,143 @@ using namespace std;
 #define vmp vector<map<int,int>>
 //typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
 
-//template <typename T>
-//using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
-const ld eps=1e-9;
-const int MOD=998244353;
-//const int MOD=1000000007;
-//const int MOD=1000003;
-//const int inf=2000000000000000000;
-const int inf=1000000000000000000LL;
-
-int mpv1(int b,int e){
-    int s=1;
-    while(e>0){
-        if(e&1) s=(s*b);
-        b=(b*b);
-        e>>=1;
-    }
-    return s;
-}
-int mp(int b,int e){
-    int s=1;
-    while(e>0){
-        if(e&1) s=(s*b)%MOD;
-        b=(b*b)%MOD;
-        e>>=1;
-    }
-    return s;
-}
-
-int dx[4]={1,-1,0,0};
-int dy[4]={0,0,1,-1};
-char mov[4]={'D','U','R','L'};
-
-const pii no={-1,-1};
-//const pii no={-inf,-inf};
-
-int lcm(int a,int b){
-    return a/__gcd(a,b)*b;
-}
-int sgn(int x){
-    if(x==0) return 0;
-    return (x<0?-1:1);
-}
-bool choque(pii a,pii b){
-    if(a>b) swap(a,b);
-    auto [l1,r1]=a;
-    auto [l2,r2]=b;
-    if(r1>=l2) return true;
-    return false;
-}
-
-int mex(vi &v){
-    int n=sz(v);
-    vi fre(n+2,0);
-    rep(i,0,n,1) if(v[i]>=0&&v[i]<=n+1) fre[v[i]]++;
-    rep(i,0,n+2,1) if(fre[i]==0) return i;
-}
-
-int damek(int x){
-    int t=0;
-    while(x%2==0){
-        x/=2;
-        t++;
-    }
-    return t;
-}
-int disth(pii a,pii b){
-    auto [a1,a2]=a;
-    auto [b1,b2]=b;
-    return abs(a1-b1)+abs(a2-b2);
-}
-bool esprimo(int n){
-    if(n==1) return false;
-    for(int i=2;i*i<=n;i++){
-        if(n%i==0) return false;
-    }
-    return true;
-}
-vi damediv(int n){
-    vi ans;
-    for(int i=1;i*i<=n;i++){
-        if(n%i) continue;
-        ans.pb(i);
-        if(i*i!=n) ans.pb(n/i);
-    }
-    return ans;
-}
-
-vi damepri(int n){
-    vi ans;
-    int nn=n;
-    for(int i=2;i*i<=n;i++){
-        if(nn%i)continue;
-        ans.pb(i);
-        while(nn%i==0) nn/=i;
-    }
-    if(nn>1) ans.pb(nn);
-    return ans;
-}
-
-bool mismarecta(pii p1,pii p2,pii p3){
-    auto [x1,y1]=p1;
-    auto [x2,y2]=p2;
-    auto [x3,y3]=p3;
-    return (((y2-y1)*(x3-x1))==((y3-y1)*(x2-x1)));
-}
-
-int disteu(pii p1,pii p2){
-    auto [x1,y1]=p1;
-    auto [x2,y2]=p2;
-    return (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
-}
-
-ld raizeu(pii p1,pii p2){
-    auto [x1,y1]=p1;
-    auto [x2,y2]=p2;
-    return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-}
-int pcruz(pii o,pii p1,pii p2){
-    auto [x1,y1]=p1;
-    auto [x2,y2]=p2;
-    auto [o1,o2]=o;
-    return (x1-o1)*(y2-o2)-(y1-o2)*(x2-o1);
-}
-
-int next2(int n){
-    int x=1;
-    while(x<n) x<<=1ll;
-    return x;
-}
-int sd(int x){
-    int t=0;
-    while(x>0){
-        t+=x%10;
-        x/=10;
-    }
-    return t;
-}
-int tam10(int x){
-    int t=0;
-    if(x==0) return 1;
-    while(x>0){
-        x/=10;
-        t++;
-    }
-    return t;
-}
-
-
-
-string sumastring(string a,string b){
-    reverse(all(a));
-    reverse(all(b));
-    int n=max(sz(a),sz(b));
-    int c=0;
-    string ans;
-    rep(i,0,n,1){
-        int x=(sz(a)<=i?0:a[i]-'0'),y=(sz(b)<=i?0:b[i]-'0');
-        int t=x+y+c;
-        char d=(t%10)+'0';
-        ans.pb(d);
-        c=t/10;
-    }
-    if(c>0) ans.pb(c+'0');
-    reverse(all(ans));
-    return ans;
-}
-
-pii intersec(pii p1,pii p2){
-    if(!choque(p1,p2)) return no;
-    auto [x1,y1]=p1;
-    auto [x2,y2]=p2;
-    int a=max(x1,x2);
-    int b=min(y1,y2);
-    return {a,b};
-}
-
-
-
-string apqpqpqpa(int x){
-    string ans;
-    nrep(j,29,0,1){
-        if((1<<j)&x) ans.pb('1');
-        else if(sz(ans))ans.pb('0');
-    }
-    return ans;
-}
-
-int raiz1(int x){
-    int k=(int)sqrt(x);
-    if(k*k==x) return k;
-    return -1;
-}
-
-pii juntar(pii p1,pii p2){
-    if(p2<p1) swap(p1,p2);
-    pii p={min(p1.first,p2.first),max(p1.second,p2.second)};
-    return p;
-}
-
-int piso(int x,int y){
-    if(x>=0) return x/y;
-    return (x-y+1)/y;
-}
-
-int techo(int x,int y){
-    if(x>=0) return (x+y-1)/y;
-    return x/y;
-}
-
-
-int conv(string x){
-    int n=sz(x),t=0;
-    rep(i,0,n,1) if(x[i]=='1')t+=(1<<i);
-    return t;
-}
-
-vi cambase(int x,int b){
-    vi v;
-    if(x==0) v.pb(0);
-    while(x>0){
-        int c=x%b;
-        v.pb(c);
-        x/=b;
-    }
-    reverse(all(v));
-    return v;
-}
-
-char xo(char c){
-    if(c=='1') return '0';
-    return '1';
-}
-
-struct cosa{
-    int l,r,x;
-    bool operator<(const cosa &o) const{
-        if(l!=o.l) return l<o.l;
-        return r<o.r;
-    }
-};
-
-struct node{
-    int a,b,c;
-};
-
-int ask(int i,int j){
-    cout<<"? "<<i<<" "<<j<<endl;
-    cout.flush();
-    int x;cin>>x;
-    return x;
-}
-
-
 
 signed main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.precision(10);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
 	cout<<fixed;
-    int tt=1;
-    cin>>tt;
-    while(tt--){
-        int s,q;cin>>s>>q;
-        vi div=damediv(s);
-        sort(all(div));
-        int n=sz(div);
-        vi pre(n,0);
-        pre[0]=s;
-        rep(i,1,n,1) pre[i]=pre[i-1]+(div[i]-div[i-1])*(s/div[i]);
-        while(q--){
-            int a=0,ans=0,x,y;cin>>x>>y;
-            auto it=upper_bound(all(div),s/y);
-            if(it!=div.begin()){
-                it--;
-                a=min(x,(*it));
-            }
-            ans=a*y;
-            if(a==x){
-                cout<<ans<<endl;
-                continue;
-            }
-            int l=lower_bound(all(div),a+1)-div.begin(),r=lower_bound(all(div),x)-div.begin();
-            if(l==r)ans+=(x-a)*(s/div[l]);
-            else ans+=(div[l]-a)*(s/div[l])+(l+1<r?pre[r-1]-pre[l]:0)+(x-div[r-1])*(s/div[r]);
-            cout<<ans<<endl;
-        }
-    }
+    int tt=1;
+    //cin>>tt;
+    while(tt--){
+        int n;cin>>n;
+        vi izq(n),der(n);
+        vc c1(n),c2(n,'?');
+        rep(i,0,n,1){
+            int a,b;char c;cin>>a>>b>>c;
+            a--;b--;
+            izq[i]=a;
+            der[i]=b;
+            c1[i]=c;
+        }
+        vb dir(n);
+        auto f=[&]()->bool{
+            nrep(i,n-1,0,1){
+                int u=izq[i],v=der[i];
+                if(u==-1) continue;
+                char k1=c1[i],k2=c2[i],u1=c1[u],u2=c2[u],v1=c1[v],v2=c2[v];
 
-    return 0;
+                if(k2=='?'){
+                    if(u1==k1||u2==k1||v1==k1||v2==k1) continue;
+                    if(u2!='?'&&v2!='?') return false;
+                    if(u2!='?'&&v1>k1) return false;
+                    if(v2!='?'&&u1>k1) return false;
+                    if(u2!='?'){
+                        c2[v]=k1;
+                        continue;
+                    }
+                    if(v2!='?'){
+                        c2[u]=k1;
+                        continue;
+                    }
+                    if(v1>k1&&u1>k1) return false;
+                    bool b1=(u1<k1),b2=(v1<k1);
+                    if((dir[i]&&b1)||(!dir[i]&&!b2)){
+                        c2[u]=k1;
+                    }else {
+                        c2[v]=k1;
+                    }
+                    continue;
+                }
+
+
+                bool o1=(u1==k1||u2==k1||v1==k1||v2==k1),o2=(u1==k2||u2==k2||v1==k2||v2==k2);
+                if(k1==k2&&o1&&o2){
+                    int t=0;
+                    if(u1==k1) t++;
+                    if(v1==k1) t++;
+                    if(u2==k1) t++;
+                    if(v2==k1) t++;
+                    if(t<2) o1=false;
+                }
+                if(o1&&o2) continue;
+                if(!o1&&!o2&&(u2!='?'||v2!='?'))  return false;
+                if(o1){
+                    if(u2!='?'&&(v1>k2||u1!=k1)) return false;
+                    if(v2!='?'&&(u1>k2||v1!=k1)) return false;
+
+                    if(u2!='?'){
+                        c2[v]=k2;
+                        continue;
+                    }
+                    if(v2!='?'){
+                        c2[u]=k2;
+                        continue;
+                    }
+                    if(v1>k2&&u1>k2) return false;
+                    bool b1=(u1<=k2&&v1==k1),b2=(v1<=k2&&u1==k1);
+                    if(!b1&&!b2) return false;
+                    if((dir[i]&&b1)||(!dir[i]&&!b2)){
+                        c2[u]=k2;
+                    }else {
+                        c2[v]=k2;
+                    }
+                }else if(o2){
+                    if(u2!='?'&&(v1>k1||u1!=k2)) return false;
+                    if(v2!='?'&&(u1>k1||v1!=k2)) return false;
+                    if(u2!='?'){
+                        c2[v]=k1;
+                        continue;
+                    }
+                    if(v2!='?'){
+                        c2[u]=k1;
+                        continue;
+                    }
+                    if(v1>k1&&u1>k1) return false;
+                    bool b1=(u1<=k1&&v1==k2),b2=(v1<=k1&&u1==k2);
+                    if(!b1&&!b2) return false;
+                    if((dir[i]&&b1)||(!dir[i]&&!b2)){
+                        c2[u]=k1;
+                    }else {
+                        c2[v]=k1;
+                    }
+                }else{
+                    o1=(k1>=u1&&k2>=v1);o2=(k2>=u1&&k1>=v1);
+                    if(!o1&&!o2) return false;
+                    if((dir[i]&&o1)||(!dir[i]&&!o2)){
+                        c2[u]=k1;
+                        c2[v]=k2;
+                    }else{
+                        c2[u]=k2;
+                        c2[v]=k1;
+                    }
+                }
+            }
+            return true;
+        };
+        bool aja=false;
+        rep(mask,0,(1ll<<n),1){
+            vc cc1=c1,cc2=c2;
+            rep(i,0,n,1){
+                if((1ll<<i)&mask) dir[i]=true;
+                else dir[i]=false;
+            }
+            if(f()){
+                aja=true;
+                break;
+            }
+            c1=cc1;
+            c2=cc2;
+        }
+        if(!aja){
+            cout<<-1<<endl;
+            continue;
+        }
+        rep(i,0,n,1){
+            if(c2[i]=='?') c2[i]='t';
+            cout<<c1[i]<<c2[i]<<endl;
+        }
+    }
+
+    return 0;
 }
